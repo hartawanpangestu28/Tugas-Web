71e6977ca6f3192e26598228e43f8c2983e41ce1
hartawanpangestu28 <hartawanpangestu02@gmail.com>
2021-10-07 13:27:04 +0800

    tugas hartawan

---
+++ frontpage/beranda.php
@@ -0,0 +1,72 @@
+<div class="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
+			<h1 class="my-4 text-3xl md:text-5xl text-purple-800 font-bold leading-tight text-center md:text-left slide-in-bottom-h1">Hartawan Musik</h1>
+			<p class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle">Ayoo kunjungi hanya disini</p>
+		
+			<p class="text-blue-400 font-bold pb-8 lg:pb-6 text-center md:text-left fade-in"></p>
+			<div class="flex w-full justify-center md:justify-start pb-24 lg:pb-0 fade-in">
+				
+			</div>
+
+		</div>
+		
+		<!--Right Col-->
+		<div class="w-full xl:w-3/5 py-6 overflow-y-hidden">
+			<img class="w-5/6 mx-auto lg:mr-0 slide-in-bottom" src="asssets/harta.jpg">
+		</div>
+
+		<div class="grid grid-cols-2 gap-2">
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+				<div class="md:flex-shrink-0">
+					<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/gitar.jpg" alt="Man looking at item at a store">
+				</div>
+				<div class="p-8">
+					<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Gitar</div>
+					<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pemesanan</button></a>
+					<p class="mt-2 text-gray-500">dengan cara dipetik</p>
+				</div>
+				</div>
+			</div>
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+					<div class="md:flex-shrink-0">
+						<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/piano.png">
+					</div>
+					<div class="p-8">
+						<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Piano</div>
+						<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pemesanan</button></a>
+						<p class="mt-2 text-gray-500">sangat merdu jika dimainkan</p>
+					</div>
+					</div>
+			</div>
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+					<div class="md:flex-shrink-0">
+						<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/terompet.jpg">
+					</div>
+					<div class="p-8">
+						<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">terompet</div>
+						<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pemesanan</button></a>
+						<p class="mt-2 text-gray-500">dengan cara ditiup</p>
+					</div>
+					</div>
+			</div>
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+					<div class="md:flex-shrink-0">
+						<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/gendang.jfif">
+					</div>
+					<div class="p-8">
+						<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">gendang</div>
+						<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pemesanan</button></a>
+						<p class="mt-2 text-gray-500">dengan cara dipukul</p>
+					</div>
+					</div>
+			</div>
+		</div>
+		<!--Footer-->
+		<div class="w-full pt-16 pb-6 text-sm text-center md:text-left fade-in">
+			<a class="text-gray-500 no-underline hover:no-underline" href="#">&copy; App 2019</a>
+		</div>
+		
+	</div>
